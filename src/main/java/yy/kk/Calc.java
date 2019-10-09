7fd1c68d340f2aa5425dabc6fbe5809c40cf58ce
wsy <237000576@qq.com>
2019-10-09 15:54:19 +0800

    tutorial5

---
+++ src/main/java/yy/kk/Calc.java
@@ -0,0 +1,11 @@
+package yy.kk;
+
+public class Calc {
+	public  int add (int a,int b)
+	{
+		return a+b;
+	}
+	public int sub(int a ,int b)
+	{return a-b;
+	}
+}
---
+++ src/test/java/yy/kk/testcal.java
@@ -0,0 +1,14 @@
+package yy.kk;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testcal {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
